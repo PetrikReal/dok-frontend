67fbf0a8e778bc2afd3cc8fc2365f30af8ac2680
Susa Milan <mailtosnek@protonmail.com>
2023-09-29 12:41:23 +0200

    Tabs don't appear on other pages

--- src/lib/components/navbar/Navbar.svelte
+++ src/lib/components/navbar/Navbar.svelte
@@ -2,12 +2,14 @@
   import Menu from './Menu.svelte';
   import Icon from './Icon.svelte';
   import { tabStore } from '$lib/tabstore';
+  import { page } from '$app/stores';
   const { tab } = tabStore;
 </script>
 
 <nav class="bg-zinc-800 text-white p-3 flex flex-row items-center justify-between px-4">
   <Icon />
 
+  {#if $page.url.pathname == '/'}
   <div class="py-2 flex flex-row rounded-full border-2 border-white">
     {#if $tab == 0}
     <p class="mx-2 bg-zinc-950 p-1 px-1.5 rounded-full">News</p>
@@ -25,6 +27,7 @@
     <button class="mx-2 bg-zinc-700 p-1 px-1.5 rounded-full" on:click={() => {tabStore.tab.set(2)}}>All Tasks</button>
     {/if}
   </div>
+  {/if}
 
   <Menu />
 </nav>
