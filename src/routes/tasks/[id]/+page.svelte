81f34b3d0aa64f4bf2b1466198ef210db87a46c1
drinkmonster <berenyibence98@gmail.com>
2023-09-30 15:45:04 +0200

    task title

--- src/routes/tasks/[id]/+page.svelte
+++ src/routes/tasks/[id]/+page.svelte
@@ -22,6 +22,7 @@
       <main class="flex justify-center">
         <div class="max-w-5xl">
           <div class="bg-zinc-800 p-4 my-5 rounded-lg text-white overflow-y-auto">
+            <h1 class="text-4xl text-center py-4 font-bold font-black">{data.title}</h1>
             <div class="flex flex-row">
               <img src={data.authorAvatar} class="w-12 h-12 mr-3 rounded-full object-cover" alt="" />
               <div class="">
