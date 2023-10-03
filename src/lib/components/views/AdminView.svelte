8dc9c579e2c12b3f609e1c070d81d5a74e005b7e
Susa Milan <mailtosnek@protonmail.com>
2023-10-03 09:28:56 +0200

    Added basic editing i will cleanup later my bluds

--- src/lib/components/views/AdminView.svelte
+++ src/lib/components/views/AdminView.svelte
@@ -4,11 +4,20 @@
   import type { RecordModel } from 'pocketbase';
   import NewTaskModal from '../modals/NewTaskModal.svelte';
   import DeleteTaskModal from '../modals/DeleteTaskModal.svelte';
+  import EditTaskModal from '../modals/EditTaskModal.svelte';
 
   let create_task_open = false;
+
   let delete_task_open = false;
   let delete_task_id = '';
   let delete_task_title = '';
+
+  // If any one of u touches these varibles i will cut ur pp :3 (i will make em disappear soon just wait my little buddies)
+  let edit_task_open = false;
+  let edit_task_id = '';
+  let edit_task_title = '';
+  let edit_task_body = '';
+  let edit_task_due_date: Date;
 
   let authored_tasks: RecordModel[] = [];
   const fetch_authored_tasks = async () => {
@@ -18,6 +27,14 @@
     });
   };
 
+  const openEditTaskModal = (event: any) => {
+    edit_task_id = event.detail.id;
+    edit_task_title = event.detail.title;
+    edit_task_body = event.detail.body;
+    edit_task_due_date = event.detail.due_date;
+    edit_task_open = true;
+  };
+
   const openDeleteTaskModal = (event: any) => {
     delete_task_id = event.detail.id;
     delete_task_title = event.detail.title;
@@ -25,6 +42,20 @@
   };
 </script>
 
+<EditTaskModal
+  on:task-edited={async () => {
+    await fetch_authored_tasks();
+    edit_task_open = false;
+  }}
+  opened={edit_task_open}
+  id={edit_task_id}
+  due_date={edit_task_due_date}
+  body={edit_task_body}
+  title={edit_task_title}
+  on:closed={() => {
+    edit_task_open = false;
+  }}
+/>
 <DeleteTaskModal
   on:close={() => {
     delete_task_open = false;
@@ -45,7 +76,7 @@
   }}
 />
 <div>
-  <div id="tasks-sec" class="max-w-[30rem] my-4 mx-auto bg-zinc-700 overflow-hidden rounded-xl">
+  <div id="tasks-sec" class="max-w-[35rem] my-4 mx-auto bg-zinc-700 overflow-hidden rounded-xl">
     <div class="p-2">
       <button
         class="px-3 p-2 bg-emerald-600 text-white font-medium rounded-lg flex-col flex mx-auto"
@@ -61,6 +92,7 @@
       {:then}
         {#each authored_tasks as task}
           <AdminTaskCard
+            on:edit-task={openEditTaskModal}
             on:delete-task={openDeleteTaskModal}
             on:task-deleted={async () => {
               await fetch_authored_tasks();
